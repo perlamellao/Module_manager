359e786a61e8b0cc7302e0daf86eb0442fdfb8f2
P3rl4 <33875901+perlamellao@users.noreply.github.com>
2017-12-30 20:38:37 +0100

    Manager V1.01
    Added new module

--- main/manager.sh
+++ main/manager.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
+Install_path="/root/Documents/pro/P3rl4_Manager/"
 
+
+
+#color variables
 bold="\033[1m"
 blink="\033[5m"
 under="\033[4m"
@@ -23,8 +27,10 @@ function init {
 		echo -e "\n\n               $green 1) $red Ver Lista De Modulos"
 		echo -e "               $green 2) $red Elegir Modulo Por Su Numero"
 		echo -e "               $green 0) $red Salir $normal \n\n\n"
+		echo -e "$blue"
 
 		read -p "P3rl4> "
+		echo -e "$normal"
 		if [[ $REPLY =~ ^[0-2]$ ]]; then
 			if [[ $REPLY == 1 ]]; then
 				funml			
@@ -40,7 +46,7 @@ function init {
 			fi
 		else	
 			clear
-			echo -e "\n\n\033[42mPon Un Numero [0-1]\033[0m"
+			echo -e "$red Pon Un Numero Valido"
 			sleep 2
 		fi
 	done
@@ -49,6 +55,7 @@ function init {
 function funml {
 	clear
 	echo -e "$red 1) $green dns_spoof $normal"
+	echo -e "$red 2) $green android_payload $normal"
 	read
 }
 #Choose Module And Execute It
@@ -56,74 +63,57 @@ function funcm {
 
 
 	clear
-	cd /root/Documents/pro/P3rl4_Manager/modules
+	cd $Install_path/modules
 
-	echo -e "\n\n$green |Que modulo quiere elegir?$normal"
+	echo -e "\n\n$green |Que modulo quiere elegir?"
 	read -p " | > " cmodule
+	echo -e "$normal"
 	
 	case $cmodule in
 		1)
+			clear
 			./dns_spoof
-			read
+			;;
+		2)
+			./android_payload
+			clear
 			;;
 		*)
+		clear
 		echo -e "$red Ese Modulo No Existe$normal"
 		sleep 2
 	esac
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-resize -s 32 68 > /dev/null
-echo -e $red""
-echo "	 									"
-echo "   	                  ..:::::::::..		"
-echo "   	               ..:::aad8888888baa:::..	"
-echo "   	           .::::d:?88888888888?::8b::::.	"
-echo "   	         .:::d8888:?88888888??a888888b:::.	"
-echo "   	       .:::d8888888a8888888aa8888888888b:::.	"
-echo "   	      ::::dP::::::::88888888888::::::::Yb::::	"
-echo "   	     ::::dP:::::::::Y888888888P:::::::::Yb::::	"
-echo "   	    ::::d8:::::::::::Y8888888P:::::::::::8b::::	"
-echo "   	   .::::88::::::::::::Y88888P::::::::::::88::::.	"
-echo "   	   :::::Y8baaaaaaaaaa88P:T:Y88aaaaaaaaaad8P:::::	"
-echo "   	   :::::::Y88888888888P::|::Y88888888888P:::::::	"
-echo "   	   ::::::::::::::::888:::|:::888::::::::::::::::	"
-echo "   	    :::::::::::::::8888888888888b::::::::::::::	"
-echo "   	    :::::::::::::::88888888888888::::::::::::::	"
-echo "   	     :::::::::::::d88888888888888::::::::::::: 	"
-echo "   	      ::::::::::::88::88::88:::88::::::::::::	"
-echo "   	        ::::::::::88::88::88:::88::::::::::	"
-echo "   	          ::::::::88::88::P::::88::::::::	"
-echo "   	            ::::::88::88:::::::88::::::	"
-echo "   	                :::::::::::::::::::	"
-echo "   	                     ::::::::: 	"
-echo -e "$normal"
-echo -e "$green Presiona [ENTER] Para Continuar"
-read
-init
+#main menu
+function main() {
+	clear
+	resize -s 32 68 > /dev/null
+	echo -e $red""
+	echo "	 									"
+	echo "   	                  ..:::::::::..		"
+	echo "   	               ..:::aad8888888baa:::..	"
+	echo "   	           .::::d:?88888888888?::8b::::.	"
+	echo "   	         .:::d8888:?88888888??a888888b:::.	"
+	echo "   	       .:::d8888888a8888888aa8888888888b:::.	"
+	echo "   	      ::::dP::::::::88888888888::::::::Yb::::	"
+	echo "   	     ::::dP:::::::::Y888888888P:::::::::Yb::::	"
+	echo "   	    ::::d8:::::::::::Y8888888P:::::::::::8b::::	"
+	echo "   	   .::::88::::::::::::Y88888P::::::::::::88::::.	"
+	echo "   	   :::::Y8baaaaaaaaaa88P:T:Y88aaaaaaaaaad8P:::::	"
+	echo "   	   :::::::Y88888888888P::|::Y88888888888P:::::::	"
+	echo "   	   ::::::::::::::::888:::|:::888::::::::::::::::	"
+	echo "   	    :::::::::::::::8888888888888b::::::::::::::	"
+	echo "   	    :::::::::::::::88888888888888::::::::::::::	"
+	echo "   	     :::::::::::::d88888888888888::::::::::::: 	"
+	echo "   	      ::::::::::::88::88::88:::88::::::::::::	"
+	echo "   	        ::::::::::88::88::88:::88::::::::::	"
+	echo "   	          ::::::::88::88::P::::88::::::::	"
+	echo "   	            ::::::88::88:::::::88::::::	"
+	echo "   	                :::::::::::::::::::	"
+	echo "   	                     ::::::::: 	"
+	echo -e "$normal"
+	echo -e "$green Presiona [ENTER] Para Continuar"
+	read
+	init
+}
+main
